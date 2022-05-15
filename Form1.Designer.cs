e87426207892e6d52d38ed225aaf60a364e390e5
Grzegorz Konopka <41437422+konopkagrzegorz@users.noreply.github.com>
2022-05-15 12:38:13 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace multithreading_separated_tasks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PrimeNumbersButton = new System.Windows.Forms.Button();
+            this.FibonacciButton = new System.Windows.Forms.Button();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.PrimeTextBox = new System.Windows.Forms.RichTextBox();
+            this.FibonacciTextBox = new System.Windows.Forms.RichTextBox();
+            this.DrawPanel = new System.Windows.Forms.Panel();
+            this.PrimesDropdown = new System.Windows.Forms.NumericUpDown();
+            this.FibonacciDropdown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.PrimesDropdown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FibonacciDropdown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PrimeNumbersButton
+            // 
+            this.PrimeNumbersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PrimeNumbersButton.Location = new System.Drawing.Point(12, 12);
+            this.PrimeNumbersButton.Name = "PrimeNumbersButton";
+            this.PrimeNumbersButton.Size = new System.Drawing.Size(220, 70);
+            this.PrimeNumbersButton.TabIndex = 0;
+            this.PrimeNumbersButton.Text = "Wyznacz liczby pierwsze";
+            this.PrimeNumbersButton.UseVisualStyleBackColor = true;
+            this.PrimeNumbersButton.Click += new System.EventHandler(this.PrimeNumbersButton_Click);
+            // 
+            // FibonacciButton
+            // 
+            this.FibonacciButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FibonacciButton.Location = new System.Drawing.Point(269, 12);
+            this.FibonacciButton.Name = "FibonacciButton";
+            this.FibonacciButton.Size = new System.Drawing.Size(220, 70);
+            this.FibonacciButton.TabIndex = 1;
+            this.FibonacciButton.Text = "Oblicz \"n\" wyraz ciągu Fibonacci\'ego";
+            this.FibonacciButton.UseVisualStyleBackColor = true;
+            this.FibonacciButton.Click += new System.EventHandler(this.FibonacciButton_Click);
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DrawButton.Location = new System.Drawing.Point(521, 12);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(220, 70);
+            this.DrawButton.TabIndex = 2;
+            this.DrawButton.Text = "Rysuj";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // PrimeTextBox
+            // 
+            this.PrimeTextBox.Location = new System.Drawing.Point(12, 136);
+            this.PrimeTextBox.Name = "PrimeTextBox";
+            this.PrimeTextBox.Size = new System.Drawing.Size(220, 313);
+            this.PrimeTextBox.TabIndex = 3;
+            this.PrimeTextBox.Text = "";
+            // 
+            // FibonacciTextBox
+            // 
+            this.FibonacciTextBox.Location = new System.Drawing.Point(269, 136);
+            this.FibonacciTextBox.Name = "FibonacciTextBox";
+            this.FibonacciTextBox.Size = new System.Drawing.Size(220, 313);
+            this.FibonacciTextBox.TabIndex = 4;
+            this.FibonacciTextBox.Text = "";
+            // 
+            // DrawPanel
+            // 
+            this.DrawPanel.Location = new System.Drawing.Point(521, 98);
+            this.DrawPanel.Name = "DrawPanel";
+            this.DrawPanel.Size = new System.Drawing.Size(220, 351);
+            this.DrawPanel.TabIndex = 5;
+            // 
+            // PrimesDropdown
+            // 
+            this.PrimesDropdown.Location = new System.Drawing.Point(112, 98);
+            this.PrimesDropdown.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.PrimesDropdown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.PrimesDropdown.Name = "PrimesDropdown";
+            this.PrimesDropdown.Size = new System.Drawing.Size(120, 20);
+            this.PrimesDropdown.TabIndex = 6;
+            this.PrimesDropdown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.PrimesDropdown.ValueChanged += new System.EventHandler(this.PrimesDropdown_ValueChanged);
+            // 
+            // FibonacciDropdown
+            // 
+            this.FibonacciDropdown.Location = new System.Drawing.Point(369, 98);
+            this.FibonacciDropdown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.FibonacciDropdown.Name = "FibonacciDropdown";
+            this.FibonacciDropdown.Size = new System.Drawing.Size(120, 20);
+            this.FibonacciDropdown.TabIndex = 7;
+            this.FibonacciDropdown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.FibonacciDropdown.ValueChanged += new System.EventHandler(this.FibonacciDropdown_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 461);
+            this.Controls.Add(this.FibonacciDropdown);
+            this.Controls.Add(this.PrimesDropdown);
+            this.Controls.Add(this.DrawPanel);
+            this.Controls.Add(this.FibonacciTextBox);
+            this.Controls.Add(this.PrimeTextBox);
+            this.Controls.Add(this.DrawButton);
+            this.Controls.Add(this.FibonacciButton);
+            this.Controls.Add(this.PrimeNumbersButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PrimesDropdown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FibonacciDropdown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PrimeNumbersButton;
+        private System.Windows.Forms.Button FibonacciButton;
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.RichTextBox PrimeTextBox;
+        private System.Windows.Forms.RichTextBox FibonacciTextBox;
+        private System.Windows.Forms.Panel DrawPanel;
+        private System.Windows.Forms.NumericUpDown PrimesDropdown;
+        private System.Windows.Forms.NumericUpDown FibonacciDropdown;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multithreading_separated_tasks
+{
+    public partial class Form1 : Form
+    {
+        int MaxPrimeNumber = 0;
+        int FibonacciNumber = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PrimeNumbersButton_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                for (int i = 0; i < MaxPrimeNumber + 1; i++)
+                {
+                    if (IsPrime(i))
+                    {
+                        PrimeTextBox.Invoke((MethodInvoker)delegate { PrimeTextBox.Text += (Environment.NewLine + i); });
+                    }
+                }
+            });
+        }
+
+
+        private bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private long CalculateFibonacci(int number)
+        {
+            if (number <= 1)
+            {
+                return number;
+            }
+            else
+            {
+                return CalculateFibonacci(number - 1) + CalculateFibonacci(number - 2);
+            }
+        }
+
+        private void PrimesDropdown_ValueChanged(object sender, EventArgs e)
+        {
+            MaxPrimeNumber = Int32.Parse(PrimesDropdown.Value.ToString());
+        }
+
+        private void FibonacciDropdown_ValueChanged(object sender, EventArgs e)
+        {
+            FibonacciNumber = Int32.Parse(FibonacciDropdown.Value.ToString());
+        }
+
+        private void FibonacciButton_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                long number = CalculateFibonacci(FibonacciNumber);
+                Invoke((MethodInvoker)delegate { FibonacciTextBox.Text += (Environment.NewLine + number); });
+            });
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                Random random = new Random();
+                for (int i = 0; i < 100; i++)
+                {
+                    DrawPanel
+                    .CreateGraphics()
+                    .DrawRectangle(new Pen(Brushes.Red, 4), new Rectangle(random.Next(0, DrawPanel.Width), random.Next(0, DrawPanel.Height), 5,5));
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+    }
+}
